179d7b8fe7f4f66a0ad59c3194dae30d72d04d49
Dan Fein <danieljfein@gmail.com>
2024-11-10 09:08:50 -0500

    latest

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,5 @@ import { ResponsiveBlackCamo } from "@/components/responsive-black-camo";
 import StaticBlackCamo from "@/components/static-camo";
 
 export default function Home() {
-  return (
-    <div>
-      <StaticBlackCamo />
-    </div>
-  );
+  return <div>Anything app</div>;
 }
